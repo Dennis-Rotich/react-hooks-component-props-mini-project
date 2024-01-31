2d2399a3b542fa2a0e5768b1242d410e903484dd
Dennis <denkipyator@gmail.com>
2024-01-31 12:15:57 +0300

    Completed assignment

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import Article from "./Article";
 
 function ArticleList(props){
-
+    const newArticle = props.posts.map((post)=>{
+        return(
+            <Article key= {post.id} title={post.title} date={post.date} preview={post.preview} />
+        )
+    })
     
     return(
         <main>
-            {props.articles.map(article => (
-                <Article
-                    key={article.title}
-                    title={article.title}
-                    date={article.date}
-                    preview={article.preview}
-                />))}
+            {newArticle}
         </main>
     )
 }
